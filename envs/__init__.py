3bfa95246660e9eb8352d49641ee59bb7d28422a
JasonMa2016 <jasonyma@seas.upenn.edu>
2022-06-05 13:20:27 -0400

    testing site

--- envs/__init__.py
+++
@@ -1,8 +0,0 @@
-import gym
-from gym.envs.registration import register
-
-def register_envs():
-    register(
-        id='DClawTurn-v0',
-        entry_point='envs.claw_env:ClawEnv',
-        max_episode_steps=80)
